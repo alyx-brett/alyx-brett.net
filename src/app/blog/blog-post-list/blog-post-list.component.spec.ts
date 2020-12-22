faacd9731852c15b27f3a0cff5b9df4d442d9e0b
Alyx Brett <alyx@alyx-brett.net>
2020-12-22 22:21:57 +0000

    dependency mangling

--- src/app/blog/blog-post-list/blog-post-list.component.spec.ts
+++ src/app/blog/blog-post-list/blog-post-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlogPostListComponent } from './blog-post-list.component';
 
@@ -6,7 +6,7 @@ describe('BlogPostListComponent', () => {
   let component: BlogPostListComponent;
   let fixture: ComponentFixture<BlogPostListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlogPostListComponent ]
     })
--- src/app/blog/blog-post-page/blog-post-page.component.spec.ts
+++ src/app/blog/blog-post-page/blog-post-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BlogPostPageComponent } from './blog-post-page.component';
 
@@ -6,7 +6,7 @@ describe('BlogPostPageComponent', () => {
   let component: BlogPostPageComponent;
   let fixture: ComponentFixture<BlogPostPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BlogPostPageComponent ]
     })
